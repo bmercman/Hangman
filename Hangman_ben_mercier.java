a542d9bcb7eca8ab3bc906d317684f959516a55a
Ben Mercier <57955502+bmercman@users.noreply.github.com>
2021-01-29 15:33:39 -0500

    Update Hangman_ben_mercier.java

--- Hangman_ben_mercier.java
+++ Hangman_ben_mercier.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hangman_ben_mercier;
 
 import java.util.Scanner;
 
 /**
  *
- * @author 070174792
+ * @author Ben Mercier
  */
 public class Hangman_ben_mercier {
 
